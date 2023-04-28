77f3c7983b7235ec65a7d5a4d9973c2782180dfa
JamesDouglas22 <20311188-JamesDouglas22@users.noreply.replit.com>
2023-04-28 17:00:40 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+/**
+*
+* Name:
+* Date:
+* Description:
+* Bugs: (if none write none known)
+* Reflection: How did you find this lab? Too easy? Too hard? 
+*
+*/
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
