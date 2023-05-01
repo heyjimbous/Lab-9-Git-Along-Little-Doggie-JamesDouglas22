33ddb68a967dac22a89e870306c7c1d39e002431
heyjimbous <116605825+heyjimbous@users.noreply.github.com>
2023-05-01 01:29:36 +0000

    Added CoolCollection<E> class: instance variables and constructor.

---
+++ gitalong/CoolCollection.java
@@ -0,0 +1,13 @@
+package gitalong;
+
+public class CoolCollection<E> {
+  private ArrayList<E> pieces;
+  private boolean iterateRandomly;
+
+  public CoolCollection(boolean iterateRandomly) {
+    
+  }
+
+
+  
+}
