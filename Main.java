28abbbcf56f84b6bc0988b3d9115164121277237
heyjimbous <116605825+heyjimbous@users.noreply.github.com>
2023-05-03 22:12:19 +0000

    Wrote Javadoc comments for the Main class.

--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
 import gitalong.*;
 import java.util.*;
+
 /**
 *
 * Name: James Douglas
 * Date: 4/28/2023
-* Description: Main class!
-* Bugs: (if none write none known)
-* Reflection: How did you find this lab? Too easy? Too hard? 
+* Description: Main class; tests the CoolCollection wrapper class and the iterators within it.
+* Bugs: None known.
+* Reflection: I found this lab to quite easy. I did have minor issues creating the inner
+* classes, but my problems were resolved after a brief review of Generics.
 *
 */
 
@@ -19,11 +21,17 @@ class Main {
     testIterator(true, phrase, "Random");
 
     // Test the well behaved iterator
-    //testIterator(false, phrase, "Well Behaved");
+    testIterator(false, phrase, "Well Behaved");
 
   }
 
-  // Tests an iterator on a CoolCollection of Strings.
+  /**
+  * Tests an iterator on a CoolCollection of Strings.
+  * 
+  * @param random boolean that determines if the CoolCollection should use a randomIterator
+  * @param phrase String that contains the phrase to process
+  * @param title String that contains a title for the output line
+  */
   public static void testIterator(boolean random, String phrase, String title) {
     
     // Create a CoolCollection, adds words from phrase
@@ -36,15 +44,25 @@ class Main {
     
   }
 
-  // Splits a phrase into words, then adds them to a collection.
-  public static void addPhrase(String phrase, CoolCollection col) {
+  /**
+  * Splits a phrase into words, then adds them to a collection.
+  *
+  * @param phrase String containing a phrase to split into words
+  * @param collection CoolCollection to add words to
+  */
+  public static void addPhrase(String phrase, CoolCollection collection) {
     String[] splitPhrase = phrase.split(" ");
     for (int i = 0; i < splitPhrase.length; i++) {
-      col.add(splitPhrase[i]);
+      collection.add(splitPhrase[i]);
     }
   }
 
-  // Prints words in a manner that matches the sample output.
+  /**
+  * Prints a line of words in a manner that matches the sample output.
+  *
+  * @param title String containing a title for the line of output.
+  * @param itr Iterator to use to iterate through the collection
+  */
   public static void printUsingIterator(String title, Iterator itr) {
     
     // Add title
@@ -54,7 +72,6 @@ class Main {
     while (itr.hasNext()) {
       System.out.printf("%s ", itr.next());
     }
-    System.out.printf("%s ", itr.next());
     System.out.printf("\n");
     
   }
